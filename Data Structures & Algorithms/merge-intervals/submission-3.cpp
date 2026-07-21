55a53acfd8ccf8f567351721e8fedcb4eb5a2d0f
Dong Chen <chdouser@outlook.com>
2026-07-21 15:43:50 +0100

    Add: merge-intervals - submission-3

---
+++ Data Structures & Algorithms/merge-intervals/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        std::sort (intervals.begin(), intervals.end(), [](const vector<int>& v1, const vector<int>& v2) {
+            return v1[0] < v2[0];
+        });
+        std::vector<vector<int>> out = {intervals[0]};
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][1] > out.back()[1] && intervals[i][0] <= out.back()[1]) {
+                out.back()[1] = intervals[i][1];
+            }
+            else if (intervals[i][0] > out.back()[1]) out.push_back(intervals[i]);
+        }
+        return out;
+    }
+};
